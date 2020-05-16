bcb94dd4047a4b736532a45f94f80feeaf1cbb90
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-05-17 00:15:43 +0100

    Initial Commit

---
+++ BasicAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicAnimations
+//
+//  Created by Denis Mordvinov on 17.05.2020.
+//  Copyright © 2020 Denis Mordvinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
