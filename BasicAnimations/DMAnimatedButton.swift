170f5af14364b6efac9983c5acad5adb29f287a9
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-05-18 21:07:35 +0100

    Touch up inside animation

--- BasicAnimations/DMAnimatedButton.swift
+++ BasicAnimations/DMAnimatedButton.swift
@@ -28,9 +28,26 @@ class DMAnimatedButton: UIButton {
         backgroundColor = .blue
         setTitle(title, for: .normal)
         titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        //self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(animateButton)))
+        addTarget(self, action: #selector(down), for: .touchDown)
+        addTarget(self, action: #selector(up), for: .touchUpInside)
+
+
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc fileprivate func down(){
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        })
+    }
+    
+    @objc fileprivate func up(){
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.transform = .identity
+        })
     }
 }
