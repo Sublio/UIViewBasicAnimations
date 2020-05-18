f08217cca5fe6e9fef6166711759ba9d25541e35
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-05-18 23:54:54 +0100

    Box for trainig

--- BasicAnimations/ViewPropertyAnimator.swift
+++ BasicAnimations/ViewPropertyAnimator.swift
@@ -11,7 +11,40 @@ import UIKit
 
 class ViewoPropertyAnimator: UIViewController {
     
+    let animator = UIViewPropertyAnimator(duration: 1, curve: .linear, animations: nil)
+    
+    var boxWidthAnchor: NSLayoutConstraint!
     
     override func viewDidLoad() {
+        view.backgroundColor = .white
+        
+        let box = UIView()
+        box.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(box)
+        boxWidthAnchor = box.widthAnchor.constraint(equalToConstant: 100)
+        boxWidthAnchor.isActive = true
+        box.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        box.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        box.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        box.backgroundColor = .red
+
+        
+        animator.addAnimations {
+            box.backgroundColor = .blue
+        }
+        
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(slider)
+        slider.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant:  -100).isActive = true
+        slider.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        slider.widthAnchor.constraint(equalToConstant: view.frame.width - 150).isActive = true
+        
+        slider.addTarget(self, action: #selector(handleSliderChanger(slider:)), for: .allEvents)
+        
+    }
+    
+    @objc fileprivate func handleSliderChanger(slider: UISlider){
+        animator.fractionComplete = CGFloat(slider.value)
     }
 }
