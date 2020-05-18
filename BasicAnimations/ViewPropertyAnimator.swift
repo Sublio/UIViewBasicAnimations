a6b5606474f4f0173504c87fcf3cd1e4709d7a94
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-05-18 23:25:27 +0100

    view animator class

---
+++ BasicAnimations/ViewPropertyAnimator.swift
@@ -0,0 +1,17 @@
+//
+//  ViewPropertyAnimator.swift
+//  BasicAnimations
+//
+//  Created by Denis Mordvinov on 18.05.2020.
+//  Copyright © 2020 Denis Mordvinov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewoPropertyAnimator: UIViewController {
+    
+    
+    override func viewDidLoad() {
+    }
+}
