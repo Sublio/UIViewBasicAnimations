4b3e6864cfa0135b130389f17b8db9190e4b9f42
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-05-18 21:22:19 +0100

    animate layout if needed

--- BasicAnimations/DMAnimatedButton.swift
+++ BasicAnimations/DMAnimatedButton.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class DMAnimatedButton: UIButton {
+    
+    fileprivate var wAnchor: NSLayoutConstraint?
+    fileprivate var hAnchor: NSLayoutConstraint?
+
     override init(frame: CGRect) {
         super.init(frame:frame)
         phaseTwo(title: "Button")
@@ -17,8 +21,11 @@ class DMAnimatedButton: UIButton {
     init(title: String) {
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
-        widthAnchor.constraint(equalToConstant: 150).isActive = true
-        heightAnchor.constraint(equalToConstant: 54).isActive = true
+        wAnchor = widthAnchor.constraint(equalToConstant: 150)
+        wAnchor?.isActive = true
+        
+        hAnchor = heightAnchor.constraint(equalToConstant: 54)
+        hAnchor?.isActive = true
         phaseTwo(title: title)
     }
     
@@ -40,14 +47,32 @@ class DMAnimatedButton: UIButton {
     }
     
     @objc fileprivate func down(){
+        wAnchor?.isActive = false
+        hAnchor?.isActive = false
+        wAnchor?.constant = 100
+        hAnchor?.constant = 100
+        
+        wAnchor?.isActive = true
+        hAnchor?.isActive = true
+        
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
-            self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            //self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            self.superview?.layoutIfNeeded()
         })
     }
     
     @objc fileprivate func up(){
+        
+        wAnchor?.isActive = false
+        hAnchor?.isActive = false
+        wAnchor?.constant = 150
+        hAnchor?.constant = 54
+        
+        wAnchor?.isActive = true
+        hAnchor?.isActive = true
+        
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
-            self.transform = .identity
+            self.superview?.layoutIfNeeded()
         })
     }
 }
